133fab7d69573b0bb888dd67ba365a2e9852c390
Biswajit Das <biswajitdas7894@gmail.com>
2022-06-05 12:43:01 +0530

    get id from url

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -3,22 +3,15 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import { Table } from "react-bootstrap";
 import Dashboard from "../Components/Dashboard/Dashboard";
-import { useLocation, useSearchParams } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import { getListSubheaderUtilityClass } from "@mui/material";
 
 const drawerWidth = 240;
 
 const Homepage = () => {
   const [users, setUsers] = useState("");
-  // const ID = useLocation().id;
-  const [searchParams, setSearchParams] = useSearchParams("");
+  const ID = useLocation().id;
 
-  if (useSearchParams.get("id")) {
-    localStorage.setItem("user-id", useSearchParams.get("id"));
-  } else {
-    searchParams.delete("id");
-    setSearchParams(searchParams);
-  }
   const getUsers = () => {
     fetch("https://payment-split-web-app.herokuapp.com/user", {
       method: "GET",
@@ -30,6 +23,9 @@ const Homepage = () => {
   };
   useEffect(() => {
     getUsers();
+    if (ID) {
+      localStorage.setItem("user-id", ID);
+    }
   }, []);
   return (
     <Box sx={{ display: "flex" }}>
