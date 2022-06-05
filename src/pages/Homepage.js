69ce093608ce43106ce8865847155e0858dee3e3
Biswajit Das <biswajitdas7894@gmail.com>
2022-06-05 13:22:38 +0530

    get id from url

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -3,15 +3,16 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import { Table } from "react-bootstrap";
 import Dashboard from "../Components/Dashboard/Dashboard";
-import { useLocation } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 import { getListSubheaderUtilityClass } from "@mui/material";
 
 const drawerWidth = 240;
 
 const Homepage = () => {
   const [users, setUsers] = useState("");
-  const location = useLocation();
-  console.log("location: ", location);
+  const queryParams = new URLSearchParams(window.location.search);
+  const ID = queryParams.get("id");
+  console.log("queryParams: ", queryParams, ID);
   const getUsers = () => {
     fetch("https://payment-split-web-app.herokuapp.com/user", {
       method: "GET",
